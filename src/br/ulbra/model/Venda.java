383ce2c8a0f83888f58686bf67bc9391f9712833
Henrique Miche Rodrigues <henrique.mrodrigues77@gmail.com>
2025-10-27 10:45:14 -0300

    Update Venda.java

--- src/br/ulbra/model/Venda.java
+++ src/br/ulbra/model/Venda.java
@@ -1,5 +1,50 @@
 package br.ulbra.model;
 
 public class Venda {
+    private int idVenda;
+    private date dataVenda;
+    private double valorTotal;
+    private Vendedor idVendedor;
 
+    public Venda() {
+    }
+
+    public Venda(int idVenda, date dataVenda, double valorTotal, Vendedor idVendedor) {
+        this.idVenda = idVenda;
+        this.dataVenda = dataVenda;
+        this.valorTotal = valorTotal;
+        this.idVendedor = idVendedor;
+    }
+
+    public int getIdVenda() {
+        return idVenda;
+    }
+
+    public date getDataVenda() {
+        return dataVenda;
+    }
+
+    public double getValorTotal() {
+        return valorTotal;
+    }
+
+    public Vendedor getIdVendedor() {
+        return idVendedor;
+    }
+
+    public void setIdVenda(int idVenda) {
+        this.idVenda = idVenda;
+    }
+
+    public void setDataVenda(date dataVenda) {
+        this.dataVenda = dataVenda;
+    }
+
+    public void setValorTotal(double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public void setIdVendedor(Vendedor idVendedor) {
+        this.idVendedor = idVendedor;
+    }
 }
