7267966f85ae21e6b81ef49ffa5e0e0db5eaceef
Henrique Miche Rodrigues <henrique.mrodrigues77@gmail.com>
2025-10-27 10:44:20 -0300

    Add Fornecedor class with getter and setter methods

--- src/br/ulbra/model/Fornecedor.java
+++ src/br/ulbra/model/Fornecedor.java
@@ -1,5 +1,61 @@
 package br.ulbra.model;
 
 public class Fornecedor {
+    private int idFornecedor;
+    private String razaoSocial;
+    private String nomeFantasia;
+    private String cnpj;
+    private String email;
+    private String telefone;
 
+    public Fornecedor() {
+    }
+
+    public int getIdFornecedor() {
+        return idFornecedor;
+    }
+
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    public String getNomeFantasia() {
+        return nomeFantasia;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setIdFornecedor(int idFornecedor) {
+        this.idFornecedor = idFornecedor;
+    }
+
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+
+    public void setNomeFantasia(String nomeFantasia) {
+        this.nomeFantasia = nomeFantasia;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
 }
